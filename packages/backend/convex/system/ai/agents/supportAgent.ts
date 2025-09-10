01614d92ba8b780cfda3f94260b7ab2dab66f23b
Mofeleng <mofelengmokgope@gmail.com>
2025-09-10 16:12:47 +0200

    19: AI Tool calling

--- packages/backend/convex/system/ai/agents/supportAgent.ts
+++ packages/backend/convex/system/ai/agents/supportAgent.ts
@@ -4,5 +4,5 @@ import { components } from "../../../_generated/api";
 
 export const supportAgent = new Agent(components.agent, {
     chat: google.chat("gemini-2.0-flash"),
-    instructions: "You are a customer support agent"
+    instructions: `You are a customer support agent. Use the "resolveConversation" tool when the user expresses finalization of the conversation. Use the "escalateConversation" when the user expresses frustration or requests a human explicitly`,
 });
